de6518c5237e729bcb95496ec85676a546dc8b6f
Sourav Ghosh <52063367+souravghosh2105@users.noreply.github.com>
2019-06-30 16:01:37 +0530

    Firebase Connected

--- app/src/main/java/com/example/govconnect/MainActivity.java
+++ app/src/main/java/com/example/govconnect/MainActivity.java
@@ -1,15 +1,42 @@
 package com.example.govconnect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView postlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout=( DrawerLayout) findViewById( R.id.draw_layout );
+        navigationView=(NavigationView)findViewById( R.id.nav_view );
+        View navView=navigationView.inflateHeaderView( R.layout.nav_header );
+
+        navigationView.setNavigationItemSelectedListener( new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                MenuItem item=null;
+                UserMenuSelector(item);
+                return false;
+            }
+        } );
+
+    }
+    private void UserMenuSelector(MenuItem item){
+
     }
     //This is a cmnt.
 }
