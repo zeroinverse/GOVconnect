321d7cce7a67bbe327251ec727a3bcfc63006b8b
zeroinverse <ujjwel17179@cse.ssn.edu.in>
2019-07-08 19:18:39 +0530

    added user setup screen.

--- app/src/main/java/com/example/govconnect/SetupActivity.java
+++ app/src/main/java/com/example/govconnect/SetupActivity.java
@@ -1,35 +1,110 @@
 package com.example.govconnect;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.example.govconnect.R;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SetupActivity extends AppCompatActivity {
 
-    private EditText userName, eMail;
+    final static int Gallery_Pick = 1;
+    String currentUserID;
+    private EditText userName, pin;
     private CircleImageView profilePic;
     private Button save;
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+    private StorageReference UserProfileImageRef;
+    private EditText UserName, FullName, CountryName;
+    private Button SaveInformationbuttion;
+    private CircleImageView ProfileImage;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID);
+        UserProfileImageRef = FirebaseStorage.getInstance().getReference().child("Profile Images");
         userName = (EditText) findViewById(R.id.setup_name);
-        eMail = (EditText) findViewById(R.id.setup_email);
-        save = (Button) findViewById(R.id.setup_button);
+        pin = (EditText) findViewById(R.id.areaPin);
+        SaveInformationbuttion = (Button) findViewById(R.id.setup_button);
         profilePic = (CircleImageView) findViewById(R.id.setup_pic);
+        loadingBar = new ProgressDialog(this);
+
+        SaveInformationbuttion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SaveAccountSetupInformation();
+            }
+        });
     }
 
+
+    private void SaveAccountSetupInformation() {
+        String user_name = userName.getText().toString();
+        String pin_code = pin.getText().toString();
+
+        if (TextUtils.isEmpty(user_name)) {
+            Toast.makeText(this, "Please write your username...", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(pin_code)) {
+            Toast.makeText(this, "Please enter your PIN code...", Toast.LENGTH_SHORT).show();
+        } else {
+            loadingBar.setTitle("Saving Information");
+            loadingBar.setMessage("Please wait, while we are creating your new Account...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
+            HashMap userMap = new HashMap();
+            userMap.put("username", user_name);
+            userMap.put("pin", pin_code);
+            userMap.put("status", "Hey there, i am a vigilant citizen.");
+            userMap.put("gender", "none");
+            userMap.put("dob", "none");
+            UsersRef.updateChildren(userMap).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if (task.isSuccessful()) {
+                        SendUserToMainActivity();
+                        Toast.makeText(SetupActivity.this, "your Account is created Successfully.", Toast.LENGTH_LONG).show();
+                        loadingBar.dismiss();
+                    } else {
+                        String message = task.getException().getMessage();
+                        Toast.makeText(SetupActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                }
+            });
+        }
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(SetupActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
 }
