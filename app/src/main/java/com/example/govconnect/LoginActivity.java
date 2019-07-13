0a8a079de24e194cc5991ff4724fdce0cfb47a87
Sourav <sourav17161@cse.ssn.edu.in>
2019-07-13 17:36:54 +0530

    Login Activity and Firebase Authentication Added

--- app/src/main/java/com/example/govconnect/LoginActivity.java
+++ app/src/main/java/com/example/govconnect/LoginActivity.java
@@ -4,19 +4,27 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.govconnect.R;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,11 +33,14 @@ public class LoginActivity extends AppCompatActivity {
     private EditText UserEmail, UserPassword;
     private TextView NeedNewAccountLink;
     private ProgressDialog loadingBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth=FirebaseAuth.getInstance();
 
         NeedNewAccountLink = (TextView) findViewById(R.id.register_account);
         UserEmail = (EditText) findViewById(R.id.login_email);
@@ -43,17 +54,70 @@ public class LoginActivity extends AppCompatActivity {
                                                   public void onClick(View view) {
                                                       SendUserToRegisterActivity();
                                                   }
-                                              }
-        );
+                                              });
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Allowlogin();
+            }
+        });
+    }
 
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+           sendusertomainactivity();
+        }
+    }
+
+
+
+    private void Allowlogin(){
+        String email=UserEmail.getText().toString();
+        String password=UserPassword.getText().toString();
+        if (TextUtils.isEmpty(email))
+        {
+       Toast.makeText(this,"Please enter your email....",Toast.LENGTH_SHORT).show();
+        }
+        else  if (TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please enter your password....",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+
+            loadingBar.setTitle("Login");
+            loadingBar.setMessage("Please wait, while we are logging into your Account...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
+         mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                                                                    @Override
+                                                                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                                                                    if(task.isSuccessful()){
+                                                                                        sendusertomainactivity();
+                                                                                        Toast.makeText(LoginActivity.this,"You are logged in succesfully...",Toast.LENGTH_SHORT).show();
+                                                                                        loadingBar.dismiss();
+                                                                                    }
+                                                                                    else {
+                                                                                        String message=task.getException().getMessage();
+                                                                                        Toast.makeText(LoginActivity.this,"Error occured : "+message,Toast.LENGTH_SHORT).show();
+                                                                                        loadingBar.dismiss();
+                                                                                    }
+                                                                                    }
+                                                                                }
+         );
+        }
+    }
+
+    private void sendusertomainactivity() {
+        Intent mainIntent = new Intent(LoginActivity.this,MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |  Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+
     }
 
     private void SendUserToRegisterActivity() {
--- app/src/main/java/com/example/govconnect/MainActivity.java
+++ app/src/main/java/com/example/govconnect/MainActivity.java
@@ -13,19 +13,27 @@ import android.view.View;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private RecyclerView postlist;
-
     private FirebaseAuth mAuth;
+    private DatabaseReference UserRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
+        mAuth=FirebaseAuth.getInstance();
+        UserRef= FirebaseDatabase.getInstance().getReference().child("Users");
+
         drawerLayout=( DrawerLayout) findViewById( R.id.draw_layout );
         navigationView=(NavigationView)findViewById( R.id.nav_view );
         View navView=navigationView.inflateHeaderView( R.layout.nav_header );
@@ -48,6 +56,35 @@ public class MainActivity extends AppCompatActivity {
         if (currentUser == null) {
             SendUserToLoginActivity();
         }
+        else
+        {
+            CheckUserExistence();
+        }
+    }
+
+    private void CheckUserExistence() {
+        final String current_user_id =mAuth.getCurrentUser().getUid();
+        UserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.hasChild(current_user_id)){
+                          SendUserToRegisterActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void SendUserToRegisterActivity() {
+        Intent RegisterIntent = new Intent(MainActivity.this, RegisterActivity.class);
+        RegisterIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(RegisterIntent);
+        finish();
     }
 
     private void SendUserToLoginActivity() {
